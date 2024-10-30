d2ef0c44a3924ff6b484db992603bf3da026d5ae
JuanAlvarado21 <juan.alvaradors@gmail.com>
2024-10-30 07:36:30 -0600

    Add files via upload

---
+++ HOSPITAL/src/consultas/Consulta.java
@@ -0,0 +1,47 @@
+package consultas;
+
+import usuarios.medicos.Medico;
+import usuarios.pacientes.Paciente;
+import consultorios.Consultorio;
+import java.time.LocalDateTime;
+
+public class Consulta {
+    public String id;
+    public LocalDateTime fechaHora;
+    public Paciente paciente;
+    public Medico medico;
+    public Consultorio consultorio;
+
+    public Consulta(String id, LocalDateTime fechaHora, Paciente paciente, Medico medico, Consultorio consultorio) {
+        this.id = id;
+        this.fechaHora = fechaHora;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.consultorio = consultorio;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public Consultorio getConsultorio() {
+        return consultorio;
+    }
+
+    public String mostrarDatos(){
+        String datos = String.format("ID: %d, FECHA/HORA: %s, PACIENTE: %s, MEDICO: %s, CONSULTORIO: %s", this.id, this.fechaHora, this.paciente.getNombre(), this.medico.getNombre(), this.consultorio.getNumeroConsultorio());
+        return datos;
+    }
+}
